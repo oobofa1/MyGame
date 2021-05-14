de318889d26c94b364036b8503ea2a89366da796
oobofa1 <puhan197117584@126.com>
2021-05-14 10:02:52 +0800

    - add all

---
+++ proj.android/gen/R.java.d
@@ -0,0 +1,6 @@
+/opt/cocos2dx/projects/MyGame/proj.android/gen/com/MyCompany/AwesomeGame/R.java \
+ : /opt/cocos2dx/projects/MyGame/proj.android/res/drawable-hdpi/icon.png \
+/opt/cocos2dx/projects/MyGame/proj.android/res/drawable-ldpi/icon.png \
+/opt/cocos2dx/projects/MyGame/proj.android/res/drawable-mdpi/icon.png \
+/opt/cocos2dx/projects/MyGame/proj.android/res/values/strings.xml \
+/opt/cocos2dx/projects/MyGame/proj.android/bin/AndroidManifest.xml \
